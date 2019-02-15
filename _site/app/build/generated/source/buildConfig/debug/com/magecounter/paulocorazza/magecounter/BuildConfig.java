c0bca2f7cce8151be4a79784387a2f06888c8144
Paulo Corazza <paulo.corazza@innovareti.com.br>
2019-02-15 01:04:43 -0200

    markdown campus party

---
+++ _site/app/build/generated/source/buildConfig/debug/com/magecounter/paulocorazza/magecounter/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.magecounter.paulocorazza.magecounter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.magecounter.paulocorazza.magecounter";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2";
+}
